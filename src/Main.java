e91e06ee9b157a3df3696a05fbd44fc43f66fd76
Derek Awender <derekawender@berkeley.edu>
2022-03-23 23:42:03 -0700

    got the input

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please list the numbers you would like to mod by. (Space separated): \n");
+        String[] mods = input.nextLine().split(" ");
+        System.out.println("\nPlease list the remainder when modding by each of the numbers already listed respectively. (Space separated):\n");
+        String[] remainders = input.nextLine().split(" ");
+
+
+    }
+}
